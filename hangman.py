5caa0ca0eddc3b8ef31d711f8edcf819ed886868
Daine Bigham <Daine.Bigham@datacom.co.nz>
2021-04-28 11:26:19 +1200

    split application into two functions and streamlined what's displayed on screen

--- hangman.py
+++ hangman.py
@@ -17,37 +17,52 @@ word = random.choice(words)
 print(f"Psst, the chosen word is {word}")
 word_length = len(word)
 blanks = ['_'] * word_length
+guesses = []
 
-win = False
-lives = 6
-count = 1
+def main(): 
+    win = False
+    count = 1
+    lives = 6
 
-while not win:
-    # prompt player to guess a letter and display current stats
-    if count != 1:
-        os.system('cls' if os.name == 'nt' else 'clear')
-    count -= 1
+    while not win:
+        # prompt player to guess a letter and display current stats
+        if count != 1:
+            os.system('cls' if os.name == 'nt' else 'clear')
+        count -= 1
+        player_screen(lives)
+        player_letter = input("\nGuess a letter: ").lower()
+        # loop through the letters in the word creating an index (i) with enumerate function
+        for i, letter in enumerate(word):
+            # if letter at current position is same as players, access blanks at current index and replace with letter
+            if letter == player_letter:
+                blanks[i] = player_letter
+        # if players guess is not in the word, lose a life and print ASCII art for hanging man
+        if player_letter not in word:
+            lives -= 1
+            guesses.append(player_letter)
+
+        # if blanks array has no more blanks, set win to True
+        if '_' not in blanks:
+            win = True
+            player_screen(lives)
+            print("You win!")
+        # if player has had 6 wrong guesses, break and end game
+        if lives == 0:
+            player_screen(lives)
+            print("You lose")
+            break
+
+# function to print the player screen 
+def player_screen(lives):
     print(hangman_art.logo)
     print(hangman_art.stages[lives])
     for blank in blanks:
-        print(blank + " ", end="")
+        print(f"{blank} ", end="")
     print("")
-    player_letter = input("\nGuess a letter: ").lower()
-    # loop through the letters in the word creating an index (i) with enumerate function
-    for i, letter in enumerate(word):
-        # if letter at current position is same as players, access blanks at current index and replace with letter
-        if letter == player_letter:
-            blanks[i] = player_letter
-    # if players guess is not in the word, lose a life and print ASCII art for hanging man
-    if player_letter not in word:
-        lives -= 1
-        input(f"The letter '{player_letter}' is not in the word, press ENTER try again...")
+    print("")
+    print("Guesses: ", end="")
+    for guess in guesses:
+        print(f"{guess} ", end="")
+    print("")
 
-    # if blanks array has no more blanks, set win to True
-    if '_' not in blanks:
-        win = True
-        print("You win!")
-    # if player has had 6 wrong guesses, break and end game
-    if lives == 0:
-        print("You lose")
-        break
+main()
